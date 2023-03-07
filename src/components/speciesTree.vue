38e2d575bc602aaf3bf609e42ed99ccb428d4882
Rohit George <rohit.george@atree.org>
2023-03-07 17:09:31 +0530

    converted to composition api

--- src/components/speciesTree.vue
+++ src/components/speciesTree.vue
@@ -1,106 +1,116 @@
 <style scoped>
-    .label{
-        display: flex;
-        align-items: center;
+    .label {
+    display: flex;
+    align-items: center;
     }
-    .label .unicon, 
-    .label span{
-        padding: 0 .25rem;
-        border-radius:15px;
-        transition: all 0.25s;
+    .label .unicon,
+    .label span {
+    padding: 0 0.25rem;
+    border-radius: 15px;
+    transition: all 0.25s;
     }
-    .label .unicon{
-        height:32;
-    }
-
-    .label.selected{
-        background: yellow;
+    .label .unicon {
+    height: 32;
     }
 
-    .label:hover{
-        background:#aaa;
-        border-radius:5px;
-        color:white;
+    .label.selected {
+    background: yellow;
+    }
+
+    .label:hover {
+    background: #aaa;
+    border-radius: 5px;
+    color: white;
     }
     .label .unicon:hover,
-    .label span:hover{
-        cursor: pointer;
+    .label span:hover {
+    cursor: pointer;
     }
 
-    .children{
-        border-left: 2px solid #aaa;
-        margin-left: 1rem;
-        padding-left: 1rem;
+    .children {
+    border-left: 2px solid #aaa;
+    margin-left: 1rem;
+    padding-left: 1rem;
     }
 
-    .fade-enter-active, .fade-leave-active {
-        transition: all 0.15s;
+    .fade-enter-active,
+    .fade-leave-active {
+    transition: all 0.15s;
     }
-    .fade-enter, .fade-leave-to {
-        opacity: 0;
+    .fade-enter,
+    .fade-leave-to {
+    opacity: 0;
     }
-
 </style>
 
 <template>
-    <div>
-        <div class="label" v-if="name && name.length" @click="toggleHideShow" :class="{selected: name == selected_taxa}">
-            <unicon name="star" fill="darkorange" width="20" height="20" hover-fill="#ff0" v-if="children.length == 0" />
-            <unicon name="folder-medical" fill="darkred" width="20" height="20" hover-fill="#ff0" v-else-if="!toggle" />
-            <unicon name="folder-minus" fill="darkgreen" width="20" height="20" hover-fill="#ff0" v-else/>
-            <span @click.stop="selectName(name)">{{ name }}</span>
-            (
-                <unicon name="search" fill="steelblue" width="12" height="12" />
-                {{node.observations}}
-                |
-                {{node.descendants}}
-                <unicon name="list-ol" fill="steelblue" width="12" height="12" />
-            )
-        </div>
-        <transition name="fade">
-            <div class="children" v-if="toggle">
-                <species-tree 
-                    v-for="child in children"
-                    :key="child.name"
-                    :node="child"
-                />
-            </div>
-        </transition>
+  <div>
+    <div
+      class="label"
+      v-if="name && name.length"
+      @click="toggleHideShow"
+      :class="{ selected: name == selected_taxa }"
+    >
+      <unicon
+        name="star"
+        fill="darkorange"
+        width="20"
+        height="20"
+        hover-fill="#ff0"
+        v-if="children.length == 0"
+      />
+      <unicon
+        name="folder-medical"
+        fill="darkred"
+        width="20"
+        height="20"
+        hover-fill="#ff0"
+        v-else-if="!toggle"
+      />
+      <unicon
+        name="folder-minus"
+        fill="darkgreen"
+        width="20"
+        height="20"
+        hover-fill="#ff0"
+        v-else
+      />
+      <span @click.stop="selectName(name)">{{ name }}</span>
+      ( <unicon name="search" fill="steelblue" width="12" height="12" />{{ node.observations }} | {{ node.descendants }} <unicon name="list-ol" fill="steelblue" width="12" height="12" /> )
     </div>
+    <transition name="fade">
+      <div class="children" v-if="toggle">
+        <SpeciesTree
+          v-for="child in children"
+          :key="child.name"
+          :node="child"
+        />
+      </div>
+    </transition>
+  </div>
 </template>
+  <script lang="ts">
+import { ref, defineComponent, computed } from "vue"
+import store from "../store/index"
 
-<script lang="ts">
-import { mapState } from 'vuex'
-import store from '../store/index'
-export default {
-    name: 'speciesTree',
-    data() {
-        return {
-            toggle: false,
-        }
+export default defineComponent({
+  name: "SpeciesTree",
+  props: {
+    node: {
+      type: Object,
+      default: () => ({ name: "", children: [] })
     },
-    props:{
-        node: {
-            type: Object,
-            default: () => ({name: "", children: []})
-        },
-    },
-    computed:{
-        ...mapState(['selected_taxa']),
-        name(){
-            return this.node.name
-        },
-        children(){
-            return this.node.children
-        }
-    },
-    methods: {
-        toggleHideShow(){
-            this.toggle = !this.toggle
-        },
-        selectName(name){
-            store.dispatch('selectTaxa', name)
-        }
-    }
-}
-</script>
+  },
+  setup(props) {
+    const toggle = ref(false);
+    const name = computed(() => props.node.name)
+    const children = computed(() => props.node.children)
+    const selected_taxa = computed(() => store.state.selected_taxa)
+
+    const toggleHideShow = () => toggle.value = !toggle.value
+    const selectName = (name) => store.dispatch("selectTaxa", name)
+
+    return { toggle, name, children, selected_taxa, toggleHideShow, selectName, }
+  },
+})
+</script> 
